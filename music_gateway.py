61191aa81973fa75874e089c84800edd11d6f2c6
Christian Bierschneider <christian.bierschneider@web.de>
2018-02-25 17:12:48 +0100

    Added new publish and subscribe topics

--- music_gateway.py
+++ music_gateway.py
@@ -10,21 +10,25 @@ def music_gateway():
     # load the mpd server depending on the operating system
     LoadMPD()
 
-    # create the MQTT intance to connect to remote broker
+    # create the MQTT instance to connect to remote broker
     mqtt = MQTT(host="mqtt.swifitch.cz", port=1883)
     mqtt.add_topics(publish_topics=[{"topic_name": "music_gateway/pub/song_playlist", "qos": 0},
                                     {"topic_name": "music_gateway/pub/player_status", "qos": 0},
-                                    {"topic_name": "music_gateway/pub/current_song",  "qos": 0}],
+                                    {"topic_name": "music_gateway/pub/current_song",  "qos": 0},
+                                    {"topic_name": "music_gateway/pub/database",      "qos": 0}],
 
                     subscribe_topics=[{"topic_name": "music_gateway/sub/song_control", "qos": 1},
-                                      {"topic_name": "music_gateway/sub/",             "qos": 1}])
+                                      {"topic_name": "music_gateway/sub/find_song",    "qos": 1}])
 
     # init of the background daemon thread
     mqtt.run()
 
     # create the MPD instance to localhost
     mpdclient = ConnectMPD("localhost", 6600)
-    mpdclient.create_music_playlist()
+    # query all songs in database
+    mqtt.publish_msgs(mpdclient.get_all_songs_in_db(), topic_name=['music_gateway/pub/database'])
+    #mpdclient.create_music_playlist()
+
     while True:
         song_playlist = mpdclient.get_current_song_playlist()
         player_status = mpdclient.get_player_status()
