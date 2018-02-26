5c05f4ab21a59a9d95f687a300e53781d4807e87
Christian Bierschneider <christian.bierschneider@web.de>
2018-02-26 16:41:26 +0100

    Added the "create_files" method to create "mpd.conf", "mpd.log", "mpd.db" files dynamically

--- src/player/load_mpd.py
+++ src/player/load_mpd.py
@@ -27,6 +27,7 @@ class LoadMPD:
         system, machine = platform.system(), platform.machine()
 
         if system == "Windows" and machine in {"i686", "i786", "x86", "x86_64", "AMD64"}:
+            self.create_files(base_path)
             mpd_exe_path = os.path.join(base_path, "music\mpd.exe")
             mpd_conf_path = os.path.join(base_path, "music\mpd.conf")
             # bsp = os.path.join(os.getcwd(), "music\mpd.exe")
@@ -45,6 +46,38 @@ class LoadMPD:
             flac_converter = os.path.join(base_path, "flac-mac")
 
         """
+
+    def create_files(self, base_path):
+        """
+        method to check if mpd.conf, mpd.log, mpd.db are exists. If not, then create these files
+
+        :param base_path, path for creating these files
+        """
+        music_directory = os.path.join(base_path, 'music\songs')
+        playlist_directory = os.path.join(base_path, 'music\\radio_playlists')
+        mpd_log_path = os.path.join(base_path, 'music\mpd.log')
+        mpd_db_path = os.path.join(base_path, 'music\mpd.db')
+        mpd_conf_path = os.path.join(base_path, 'music\mpd.conf')
+
+        if not os.path.exists(mpd_log_path):
+            open(mpd_log_path, 'w')
+        if not os.path.exists(mpd_db_path):
+            open(mpd_db_path, 'w')
+        if not os.path.exists(mpd_conf_path):
+            with open(mpd_conf_path, 'w') as mpd_conf:
+                mpd_conf.writelines("music_directory " + "\"" + music_directory + "\"" + "\n" +
+                                    "playlist_directory " + "\"" + playlist_directory + "\"" + "\n" +
+                                    "log_file " + "\"" + mpd_log_path + "\"" + "\n" +
+                                    "db_file " + "\"" + mpd_db_path + "\"" + "\n" + "\n")
+
+                mpd_conf.writelines("audio_output {" + "\n" + "    type \"winmm\"" + "\n" + "    name \"Speakers\"" + "\n" +
+                                    "    device \"Lautsprecher (High Definition Audio-Gerät)\"" + "\n" + "}" + "\n")
+
+                mpd_conf.writelines("audio_output {" + "\n" + "    type \"httpd\"" + "\n" + "    name \"My HTTP Stream\"" + "\n"
+                                      "    encoder \"vorbis\" # optional, vorbis or lame" + "\n" + "    port \"8000\"" + "\n"
+                                    + "    # quality \"5.0\" # do not define if bitrate is defined" + "\n"
+                                    + "    bitrate \"128\" # do not define if quality is defined" + "\n"
+                                    + "    format \"44100:16:1\"" + "\n" + "}")
 
     @staticmethod
     def is_mpd_running():
@@ -78,4 +111,6 @@ class LoadMPD:
 
         :return:
         """
+
+        #self.mpd_pid = subprocess.Popen([mpd_exe_path, mpd_conf_path], creationflags=DETACHED_PROCESS).pid
 
