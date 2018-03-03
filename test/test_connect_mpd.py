da4bae35007d9c0707306e4a62f20df2ec2536fa
Christian Bierschneider <christian.bierschneider@web.de>
2018-03-03 23:13:39 +0100

    Added first lines in test classes

---
+++ test/test_connect_mpd.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestConnectMPD(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_gps.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestGPS(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
