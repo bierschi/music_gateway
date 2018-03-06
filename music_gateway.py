dfc6c25a4149420ade964481053cd6aa3c5d92f4
Christian Bierschneider <christian.bierschneider@web.de>
2018-03-06 22:24:54 +0100

    Deleted some topics. Added new log format

--- music_gateway.py
+++ music_gateway.py
@@ -6,26 +6,27 @@ from src.player.connect_mpd import ConnectMPD
 
 from time import sleep
 import logging as log
+import json
 
 verbose = True
 
 if verbose:
-    log.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s     -     FUNCNAME-FILE: %(funcName)s - "
-                           "%(filename)""s ", level=log.DEBUG)
+    log.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=log.DEBUG)
 else:
     log.basicConfig(format="%(levelname)s: %(message)s")
 
+def test():
+    LoadMPD()
 
 def music_gateway():
     # load the mpd server depending on the operating system
     LoadMPD()
 
     # create the MQTT instance to connect to remote broker
-    mqtt = MQTT(host="mqtt.swifitch.cz", port=1883)
-    mqtt.add_topics(publish_topics=[{"topic_name": "music_gateway/pub/song_playlist", "qos": 0},
-                                    {"topic_name": "music_gateway/pub/player_status", "qos": 0},
-                                    {"topic_name": "music_gateway/pub/current_song",  "qos": 0},
-                                    {"topic_name": "music_gateway/pub/database",      "qos": 0}],
+    #mqtt = MQTT(host="mqtt.swifitch.cz", port=1883)
+    mqtt = MQTT(host="bierschneider.ddns.net", port=1883)
+    mqtt.add_topics(publish_topics=[{"topic_name": "music_gateway/pub/database", "qos": 0},
+                                    {"topic_name": "music_gateway/pub/playback", "qos": 0}],
 
                     subscribe_topics=[{"topic_name": "music_gateway/sub/song_control", "qos": 1},
                                       {"topic_name": "music_gateway/sub/find_song",    "qos": 1}])
@@ -37,15 +38,19 @@ def music_gateway():
     mpdclient = ConnectMPD("localhost", 6600)
     # query all songs in database
     mqtt.publish_msgs(mpdclient.get_all_songs_in_db(), topic_name=['music_gateway/pub/database'])
+
     #mpdclient.create_music_playlist()
+    #mpdclient.clear_current_playlist()
 
     while True:
         song_playlist = mpdclient.get_current_song_playlist()
         player_status = mpdclient.get_player_status()
         current_song = mpdclient.get_current_song()
-        mqtt.publish_msgs(song_playlist, topic_name=['music_gateway/pub/song_playlist'])
-        mqtt.publish_msgs(player_status, topic_name=['music_gateway/pub/player_status'])
-        mqtt.publish_msgs(current_song,  topic_name=['music_gateway/pub/current_song'])
+        mqtt.publish_msgs({'current_song': current_song, 'song_playlist': song_playlist, 'player_status': player_status},
+                          topic_name=['music_gateway/pub/playback'])
+        #mqtt.publish_msgs(song_playlist, topic_name=['music_gateway/pub/song_playlist'])
+        #mqtt.publish_msgs(player_status, topic_name=['music_gateway/pub/player_status'])
+        #mqtt.publish_msgs(current_song,  topic_name=['music_gateway/pub/current_song'])
         sleep(1)
 
 
