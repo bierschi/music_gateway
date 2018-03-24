15408844aa83d1d529379fe2529859ed64c10f0c
Christian Bierschneider <christian.bierschneider@web.de>
2018-03-24 21:53:07 +0100

    Added new variable names and added description for methods

--- src/communication/scan_serial.py
+++ src/communication/scan_serial.py
@@ -6,15 +6,14 @@ import errno
 
 class ScanSerial:
 
-    _TRY_BAUDS = [4800, 7200, 9600, 14400, 19200, 38400, 57600, 115200]
-
-    def __init__(self, baudrate=None):
+    def __init__(self):
         """
 
         """
         self.available = []
         self.possible_ports = []
-        self.gps_nmea = [b'$GPRMC', b'$GPGGA', b'$GPGSA']
+        self.serial_port = None
+        self.gps_dataformats = [b'$GPRMC', b'$GPGGA', b'$GPGSA']
 
         if sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
             self.possible_ports += glob.glob("/dev/rfcomm[0-9]*")
@@ -30,24 +29,19 @@ class ScanSerial:
             ]
             self.possible_ports += [port for port in glob.glob('/dev/tty.*') if port not in exclude]
 
-        """   
-        try:
-            self.set_baudrate(baudrate)
-        except:
-            raise ValueError
-        """
-
     def get_gps_port(self):
         """
+        returns the port where the gps is connected
 
-        :return:
+        :return: gps port as string
         """
         for port in self.available_ports():
-            self.port = serial.Serial(port=port)
-            line = self.port.readline()
-            for gps in self.gps_nmea:
-                if gps in line:
-                    self.port.close()
+            self.serial_port = serial.Serial(port=port)
+            self.serial_port.timeout = 1
+            line = self.serial_port.readline()
+            for dataformat in self.gps_dataformats:
+                if dataformat in line:
+                    self.serial_port.close()
                     return port
 
     def try_port(self, port_str):
@@ -56,13 +50,13 @@ class ScanSerial:
         """
         try:
             s = serial.Serial(port_str)
-            s.close()  # explicit close 'cause of delayed GC in java
+            s.close()
             return True
 
         except serial.SerialException:
             pass
         except OSError as e:
-            if e.errno != errno.ENOENT: # permit "no such file or directory" errors
+            if e.errno != errno.ENOENT:
                 raise e
 
         return False
@@ -79,32 +73,3 @@ class ScanSerial:
 
         return self.available
 
-    def set_baudrate(self, baudrate):
-        """
-
-        :param baudrate:
-        :return:
-        """
-        if baudrate is None:
-            self.baudrate = self.auto_baudrate()
-        else:
-            self.baudrate = baudrate
-
-    def auto_baudrate(self):
-        """
-
-        :return:
-        """
-        gps_nmea = {b'$GPRMC', b'$GPGGA'}
-        self.port.timeout = 1
-        for baud in self._TRY_BAUDS:
-            self.port.baudrate = baud
-
-
-if __name__ == '__main__':
-    ports = ScanSerial()
-    #ports = ports.available_ports()
-    #print(ports)
-    #ser = serial.Serial(ports[0], baudrate=9600)
-    #while True:
-    #    print(ser.readline())
