297d8237c9e24ce3bd438c78ca92ddbf6728b771
Christian Bierschneider <christian.bierschneider@web.de>
2018-03-29 17:46:47 +0200

    Added test file test_scan_serial.py

---
+++ test/test_scan_serial.py
@@ -0,0 +1,24 @@
+import unittest
+from src.communication.scan_serial import ScanSerial
+
+
+class TestScanSerial(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_gps_port(self):
+        pass
+
+    def test_try_port(self):
+        pass
+
+    def test_available_ports(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
