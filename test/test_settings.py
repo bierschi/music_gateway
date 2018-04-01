1c14afbf184af7aa3474e1a530eceb12ca1c28bf
Christian Bierschneider <christian.bierschneider@web.de>
2018-04-01 18:14:32 +0200

    Added test file test_settings.py

---
+++ test/test_settings.py
@@ -0,0 +1,27 @@
+import unittest
+from src.settings import Settings
+
+
+class TestSettings(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_center(self):
+        pass
+
+    def test_create_json(self):
+        pass
+
+    def test_fetch(self):
+        pass
+
+    def test_makeform(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
